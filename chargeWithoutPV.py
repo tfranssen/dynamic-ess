35dc29fbd39f9551a755acc34ba3428bd42c3a54
tfranssen <thijsfranssen@gmail.com>
2022-12-13 20:10:45 +0100

    Added Logger and Scheduler

--- chargeWithoutPV.py
+++ chargeWithoutPV.py
@@ -13,37 +13,85 @@
 #  
 #  Retrieving data is now only available for ANWB, other providers will be added later. 
 
-import requests, datetime, pytz, base64, time
+import requests, datetime, pytz, base64, time, logzero, schedule
 from datetime import datetime, timedelta
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from secret import password, vrmID, username
 import paho.mqtt.client as mqtt
+from logzero import logger
+
+global dfPrices, flagConntected, lowChargeLimit, dateToday, tz, plotImage, defaultGridSetpoint, chargingGridSetpoint, password, vrmID, username, lastChargeCondition
+from secret import password, vrmID, username
 
 # Settings
 lowChargeLimit = 0.8
 dateToday = 1; # If 1, date is today, if 0 date is tomorrow, for testing only
 tz = "Europe/Amsterdam" # Time zone
+plotImage = 0 # If true image get created
+defaultGridSetpoint = 30 # Default grid point (Watt)
+chargingGridSetpoint = 3000 # Charging grid point (Watt)
+lastChargeCondition = 0
+
+# Secrets
 # vrmID = "" # VRM ID, if not imported from secret.py put it here
 # username = "" #VRM Username, if not imported from secret.py put it here
 # Save base64 encoded password to sectret.py. 
 # For testing you can write your password here in plain text but not recommended
 password = base64.b64decode(password).decode("utf-8") # Retrieve password from secrets.py
-plotImage = 0 # If true image get created
-defaultGridSetpoint = 30 # Default grid point (Watt)
-chargingGridSetpoint = 3000 # Charging grid point (Watt)
+
+# Set up MQTT Broker
+global client 
+
+def on_connect(client, userdata, flags, rc):
+    client.subscribe("$SYS/#")
+    global flagConntected
+    flagConntected = 1 
+    logger.info("Broker connected.")
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    
+def on_publish(client, userdata, mid):
+    logger.info("Message Published.")    
+    
+def on_disconnect(client, userdata, rc):
+    global flagConntected
+    flagConntected = 0  
+    logger.info("Broker disconnected.")
+
+client = mqtt.Client("client12312313")
+flagConntected = 0
+client.on_connect = on_connect
+client.on_message = on_message
+client.on_publish = on_publish
+client.on_disconnect = on_disconnect
+print("Connecting to broker")
+client.tls_set("venus-ca.crt")
+client.username_pw_set(username, password)       
+
+# Declare logfile
+logzero.logfile("log.log")
 
 # Calculate MQTT Broker URL
-sum2 = 0
-for character in vrmID.lower().strip():
-    sum2 += ord(character)
-broker_index = sum2 % 128
-brokerURL = "mqtt{}.victronenergy.com".format(broker_index)
 
-while True:
+def calculateBroker(vrmID):
+    sum2 = 0
+    for character in vrmID.lower().strip():
+        sum2 += ord(character)
+    broker_index = sum2 % 128
+    brokerURL = "mqtt{}.victronenergy.com".format(broker_index)
+    return brokerURL
 
+# Retrieve ANWB prices
+
+def getPrices():
     # Retrieve prices
+    global dfPrices, averagePrice
+    if dateToday:
+        dayString = "today"
+    else:
+        dayString = "tomorrow"
     fromDate = datetime.now().replace(microsecond=0, second=0, minute=0, hour=0)
     tillDate = datetime.now().replace(microsecond=0, second=0, minute=0, hour=0)+timedelta(days=1)-timedelta(seconds=1)
     if dateToday == 0:
@@ -63,6 +111,8 @@ while True:
     dfPrices['localDate'] = dfPrices['readingDate'].dt.tz_convert(tz)
     averagePrice = round(dfPrices["price"].mean()*100)/100
     dfPrices['chargeCondition'] = np.where((dfPrices['price'] < averagePrice*lowChargeLimit), True, False)
+    logger.info("Now prices retrieved for " + dayString)
+    logger.info("Average price is: " + str(averagePrice))
 
     #Plot prices
     if plotImage:
@@ -72,10 +122,6 @@ while True:
         plt.bar(x,y, color=colors)
         plt.xlim(-1,24)
         plt.grid(1)
-        if dateToday:
-            dayString = "today"
-        else:
-            dayString = "tomorrow"
         plt.title("Energy prices " + dayString + ". Mean price: €" + str(round(dfPrices["price"].mean()*100)/100))
         plt.xlabel("Hour")
         plt.ylabel("Price (€)")
@@ -84,54 +130,57 @@ while True:
         plt.legend(["Average", "Lower limit", "Charging hours"])
         plt.savefig('plot.png')
 
+def updateController():
+    global flagConntected, client
+
     # Find current price
     now = datetime.now().replace(microsecond=0, second=0, minute=0)
     nowTZ = now.astimezone(pytz.timezone(tz))
     chargeConditionNow = dfPrices["chargeCondition"].loc[dfPrices['localDate'] == nowTZ].item()
     chargePriceNow = dfPrices["price"].loc[dfPrices['localDate'] == nowTZ].item()
 
-    # Control ESS over MQTT
-    flag_connected = 0 
-    def on_connect(client, userdata, flags, rc):
-        print("Connected with result code "+str(rc))
-        client.subscribe("$SYS/#")
-        global flag_connected
-        flag_connected = 1
 
-    def on_message(client, userdata, msg):
-        print(msg.topic+" "+str(msg.payload))
-        
-    def on_publish(client, userdata, mid):
-        print("Message Published")
-        
-    def on_disconnect(client, userdata, rc):
-        global flag_connected
-        flag_connected = 0    
-        
-    client = mqtt.Client("client12312313")
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.on_publish = on_publish
-    client.on_disconnect = on_disconnect
-
-    print("Connecting to broker")
-    client.tls_set("venus-ca.crt")
-    client.username_pw_set(username, password)
-    client.connect(brokerURL, 8883, keepalive=60)
-    client.loop_start()
 
     # Wait for connecting
-    while not flag_connected:
+    while not flagConntected:
         time.sleep(1)
 
-    if chargeConditionNow:
-        if flag_connected:
-            client.publish("W/" + vrmID + "/settings/0/Settings/CGwacs/AcPowerSetPoint", '{"value":' + str(chargingGridSetpoint) + '}')
-            print ("Current price is €" + str(chargePriceNow) + ". The average price today is €" + str(averagePrice) + ". This is lower then " + str(lowChargeLimit) + "* daily average so the battery is now charging.")
-    else:
-        if flag_connected:
-            client.publish("W/" + vrmID + "/settings/0/Settings/CGwacs/AcPowerSetPoint", '{"value":' + str(defaultGridSetpoint) + '}')
-            print ("Current price is €" + str(chargePriceNow) + ". The average price today is €" + str(averagePrice) + ". This is not low enough to start charging. ")
+    if chargeConditionNow != lastChargeCondition:
+        logger.info("Requirement has changed, sending MQTT message to change setpoint.")
 
-    client.loop_stop()
-    time.sleep(300)
+        # Control ESS over MQTT
+        client.connect(calculateBroker(vrmID), 8883, keepalive=60)
+        client.loop_start()
+
+        if chargeConditionNow:
+        # If the ESS should charge do this:        
+            if flagConntected:
+                client.publish("W/" + vrmID + "/settings/0/Settings/CGwacs/AcPowerSetPoint", '{"value":' + str(chargingGridSetpoint) + '}')
+                logger.info("Current price is €" + str(chargePriceNow) + ". The average price today is €" + str(averagePrice) + ". This is lower then " + str(lowChargeLimit) + "* daily average so the battery is now charging.")
+                lastChargeCondition = 1                
+        else:
+        # If the ESS should not charge do this:              
+            if flagConntected:
+                client.publish("W/" + vrmID + "/settings/0/Settings/CGwacs/AcPowerSetPoint", '{"value":' + str(defaultGridSetpoint) + '}')
+                logger.info("Current price is €" + str(chargePriceNow) + ". The average price today is €" + str(averagePrice) + ". This is not low enough to start charging. ")
+                lastChargeCondition = 0
+        
+        client.loop_stop()
+    logger.info("Requirement has not changed. No MQTT message needed. ")        
+# Set up scheduler
+schedule.every().day.at("00:00:05").do(getPrices)
+schedule.every(5).minutes.do(updateController)
+logger.info("Get prices is scheduled every day at 00:00:05.")
+logger.info("The ESS controller is scheduled every 5 minutes.")
+
+getPrices()
+updateController()
+
+def main():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
+
