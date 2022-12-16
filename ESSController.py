d99371fc199948453dff1feb51ab64e2443447a4
tfranssen <thijsfranssen@gmail.com>
2022-12-16 13:16:21 +0100

    Added new charge / discharge modes

--- ESSController.py
+++ ESSController.py
@@ -3,7 +3,7 @@
 
 # Simple script for charging battery when prices are low
 # In this scenario the battery will be charged when prices are x percent below the dialy average. 
-# The limit can be set by the lowChargeLimit variable. 
+# The limit can be set by the lowThreshold variable. 
 # Default is 0.8, which means that charging will start when current price is 20% below the daily average. 
 #
 # To get this script working you need to provide (best to edit the secret.py file):
@@ -17,30 +17,22 @@ import requests, datetime, pytz, base64, time, logzero, schedule, xmltodict
 from datetime import datetime, timedelta
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from matplotlib.lines import Line2D
 import numpy as np
 import paho.mqtt.client as mqtt
 from logzero import logger
 
-global dfPrices, flagConntected, lowChargeLimit, dateToday, tz, plotImage, defaultGridSetpoint, chargingGridSetpoint, password, vrmID, username, lastChargeCondition, PV, locLat, locLong, angle, direction , totPower
+global dfPrices, flagConntected, lowThreshold, highThreshold, dateToday, tz, plotImage, defaultGridSetpoint, chargingGridSetpoint, password, vrmID, username, lastChargeCondition, lastDischargeCondition, PV, locLat, locLong, angle, direction , totPower, chargeMode, chargeHours
 from secret import password, vrmID, username
 
 # Settings
-lowChargeLimit = 0.8
 dateToday = 1; # If 1, date is today, if 0 date is tomorrow, for testing only
 tz = "Europe/Amsterdam" # Time zone
 plotImage = 1 # If true image get created
 defaultGridSetpoint = 30 # Default grid point (Watt)
 chargingGridSetpoint = 3000 # Charging grid point (Watt)
-lastChargeCondition = 0
 provider = 1 #0 = ANWB, 1 = ENTSOE
-
-#PV Settings
-PV = 0 # if PV = 1, PV Aware charging is enabled. PV = 0 is off
-locLat = "51.33.36" # Latitude
-locLong = "5.5.60" # Longitude
-angle = 40 # Angle of your panels 0 (horizontal) … 90 (vertical)
-direction = 90 # Plane azimuth, -180 … 180 (-180 = north, -90 = east, 0 = south, 90 = west, 180 = north)
-totPower = 3 # installed modules power in kilo watt
 
 # Secrets
 # vrmID = "" # VRM ID, if not imported from secret.py put it here
@@ -48,6 +40,26 @@ totPower = 3 # installed modules power in kilo watt
 # Save base64 encoded password to sectret.py. 
 # For testing you can write your password here in plain text but not recommended
 password = base64.b64decode(password).decode("utf-8") # Retrieve password from secrets.py
+
+# Buy & sell modes
+# 1 = Charge when current price < X average price
+# 2 = Charge when current price < X average price
+# 3 = Charge during cheapest x hours
+chargeMode = 3
+# Mode 1 settings
+lowThreshold = 0.8 # Constant to set the low threshold for charging
+# Mode 2 settings
+highThreshold = 1.2 # Constant to set the high threshold for selling
+# Mode 3 settings
+chargeHours = 5
+
+#PV Settings
+PV = 1 # if PV = 1, PV Aware charging is enabled. PV = 0 is off
+locLat = "51.33.36" # Latitude
+locLong = "5.5.60" # Longitude
+angle = 40 # Angle of your panels 0 (horizontal) … 90 (vertical)
+direction = 90 # Plane azimuth, -180 … 180 (-180 = north, -90 = east, 0 = south, 90 = west, 180 = north)
+totPower = 3 # installed modules power in kilo watt
 
 # Set up MQTT Broker
 global client 
@@ -78,7 +90,7 @@ client.on_disconnect = on_disconnect
 client.tls_set("venus-ca.crt")
 client.username_pw_set(username, password)       
 
-# Declare logfile
+# Setup logfile
 logzero.logfile("log.log", maxBytes=1e6, backupCount=3)
 
 # Calculate MQTT Broker URL
@@ -89,6 +101,10 @@ def calculateBroker(vrmID):
     broker_index = sum2 % 128
     brokerURL = "mqtt{}.victronenergy.com".format(broker_index)
     return brokerURL
+
+# Init misc vars
+lastChargeCondition = 0
+lastDischargeCondition = 0
 
 # Retrieve prices
 def getPrices():
@@ -136,7 +152,6 @@ def getPrices():
         # Calculate average price
         averagePrice = round(dfPrices["price"].mean()*100)/100
         # Add column with charge condition based on lower limit.
-        dfPrices['chargeCondition'] = np.where((dfPrices['price'] < averagePrice*lowChargeLimit), True, False)
         logger.info("Now prices retrieved for " + dayString)
         logger.info("Average price is: €" + '%.2f' % averagePrice)
     
@@ -174,9 +189,8 @@ def getPrices():
         dfPrices.rename(columns={'price.amount': 'price'}, inplace=True)
         dfPrices.drop('position', axis=1, inplace=True)
         averagePrice = round(dfPrices["price"].mean()*100)/100
-        dfPrices['chargeCondition'] = np.where((dfPrices['price'] < averagePrice*lowChargeLimit), True, False)
         logger.info("Now prices retrieved for " + dayString)
-        logger.info("Average price is: €" + str(averagePrice))
+        logger.info("Average price is: €" + '%.2f' % averagePrice)
 
     if PV:
         url = "https://api.forecast.solar/estimate/"+str(locLat)+"/"+str(locLong)+"/"+str(angle)+"/"+str(direction)+"/"+str(totPower)+"?no_sun=1"
@@ -208,7 +222,14 @@ def getPrices():
         y = dfPrices.price.tolist()
         fig, ax1 = plt.subplots() 
         ax1.set_ylabel('Price (€)') 
-        colors = ["red" if i <= averagePrice*lowChargeLimit else "blue" for i in y]
+        colors = []
+        for i in range(len(y)):
+            if y[i] <= averagePrice*lowThreshold:
+                colors.append("red")
+            elif y[i] > averagePrice*highThreshold:
+                colors.append("green")
+            else:
+                colors.append("blue")
         plot_1 = ax1.bar(x, y, color = colors) 
         ax1.tick_params(axis ='y', labelcolor = 'black') 
         plt.xlim(-1,24)
@@ -217,58 +238,118 @@ def getPrices():
             dayString = "today"
         else:
             dayString = "tomorrow"
-        timestr = time.strftime("%Y - %m - %d")
-        plt.title("Energy prices " + dayString + " " + timestr + ". Mean price: €" + str(round(dfPrices["price"].mean()*100)/100))
+        if provider == 0:
+            providerString = "ANWB"
+        elif provider == 1:
+            providerString = "ENTSO-e"            
+        timestr = time.strftime("%Y-%m-%d")
+        plt.title("Energy prices " + dayString + " " + timestr + ". Mean price: €" + '%.2f' % averagePrice + "\nEnergy provider: " +providerString)
         ax1.axhline(y=averagePrice, color="green")
-        ax1.axhline(y=averagePrice*lowChargeLimit, color="red")
-        ax1.legend(["Average", "Lower limit", "Charging hours","Forecast"])
-        ax1.set_xlabel('Hour')     
+        ax1.axhline(y=averagePrice*lowThreshold, color="red")
+        ax1.axhline(y=averagePrice*highThreshold, color="red")
+
+        # Create legend
+        handles, labels = plt.gca().get_legend_handles_labels()
+        patch = mpatches.Patch(color='red', label='Charge hours')  
+        patch1 = mpatches.Patch(color='green', label='Sell hours')    
+        patch2 = mpatches.Patch(color='blue', label='Normal ESS hours')            
+        line = Line2D([0], [0], label='Average €' + '%.2f' % averagePrice, color='green')
+        lowThresholdPrice = averagePrice*lowThreshold
+        highThresholdPrice = averagePrice*highThreshold
+        line1 = Line2D([0], [0], label='Low threshold: €' + '%.2f' % lowThresholdPrice , color='red')
+        line2 = Line2D([0], [0], label='High threshold: €' + '%.2f' % highThresholdPrice , color='red')
+        handles.extend([patch, patch1, patch2, line, line1, line2])
+        ax1.set_xlabel('Hour')    
         if PV:
             y2 = dfPrices.wattHours.tolist()                    
             ax2 = ax1.twinx() 
             ax2.set_ylabel('Energy (Wh)') 
             plot_2 = ax2.plot(x, y2, color = 'yellow') 
             ax2.tick_params(axis ='y')         
-            ax2.legend(["Forecast"])
+            line3 = Line2D([0], [0], label='Forecast' , color='yellow')      
+            handles.extend([line3])
+        ax1.legend(handles=handles, loc="lower left")
         timestr = time.strftime("%Y%m%d")
         fig.savefig("plot-" + timestr + ".png")
         logger.info("New plot created and saved. Filename: " + "plot-" + timestr + ".png")
 
+    # Set charge and sell labels
+    if chargeMode == 1:
+        dfPrices['chargeCondition'] = np.where((dfPrices['price'] < averagePrice*lowThreshold), True, False)
+    if chargeMode == 2:
+        dfPrices['chargeCondition'] = np.where((dfPrices['price'] < averagePrice*lowThreshold), True, False)
+        dfPrices['dischargeCondition'] = np.where((dfPrices['price'] >= averagePrice*highThreshold), True, False)
+    if chargeMode == 3:
+        dfPrices.sort_values(by=['price'], inplace=True)
+        dfPrices = dfPrices.reset_index(drop=True)
+        dfPrices['sorting'] = dfPrices.index
+        dfPrices['chargeCondition'] = np.where((dfPrices['sorting'] < chargeHours), True, False)        
+
+
 def updateController():
-    global flagConntected, client, lastChargeCondition
+    global flagConntected, client, lastChargeCondition, lastDischargeCondition, lowThreshold, highThreshold
 
     # Find current price and required charge condition
     now = datetime.now().replace(microsecond=0, second=0, minute=0)
     nowTZ = now.astimezone(pytz.timezone(tz))
-    chargeConditionNow = dfPrices["chargeCondition"].loc[dfPrices['localDate'] == nowTZ].item()
     chargePriceNow = dfPrices["price"].loc[dfPrices['localDate'] == nowTZ].item()
 
-    if chargeConditionNow != lastChargeCondition:
-        logger.info("Requirement has changed, sending MQTT message to change setpoint.")
+    if chargeMode == 1 or 2:
+        chargeConditionNow = dfPrices["chargeCondition"].loc[dfPrices['localDate'] == nowTZ].item()
+    if chargeMode == 2:
+        dischargeConditionNow = dfPrices["dischargeCondition"].loc[dfPrices['localDate'] == nowTZ].item()
 
-        # Control ESS over MQTT
-        client.connect(calculateBroker(vrmID), 8883, keepalive=60)
-        client.loop_start()
-
-        # Wait for connecting
-        while not flagConntected:
-            time.sleep(1)
-
-        if chargeConditionNow:
-        # If the ESS should charge do this:        
-            if flagConntected:
-                client.publish("W/" + vrmID + "/settings/0/Settings/CGwacs/AcPowerSetPoint", '{"value":' + str(chargingGridSetpoint) + '}')
-                logger.info("Current price is €" + '%.2f' % chargePriceNow + ". The average price today is €" + '%.2f' % averagePrice + ". This is lower than " + str(lowChargeLimit) + " * daily average so the battery is now charging.")
-                lastChargeCondition = 1                
+    if chargeMode == 1 or 2 or 3:
+        if chargeConditionNow != lastChargeCondition:
+            logger.info("Requirement has changed, sending MQTT message to change setpoint.")
+            # Control ESS over MQTT
+            client.connect(calculateBroker(vrmID), 8883, keepalive=60)
+            client.loop_start()
+            # Wait for connecting
+            while not flagConntected:
+                time.sleep(1)
+            if chargeConditionNow:
+            # If the ESS should charge do this:        
+                if flagConntected:
+                    client.publish("W/" + vrmID + "/settings/0/Settings/CGwacs/AcPowerSetPoint", '{"value":' + str(chargingGridSetpoint) + '}')
+                    logger.info("Current price is €" + '%.2f' % chargePriceNow + ". The average price today is €" + '%.2f' % averagePrice + ". This is lower than " + str(lowThreshold) + " * daily average so the battery is now charging.")
+                    lastChargeCondition = 1                
+            else:
+            # If the ESS should not charge do this:              
+                if flagConntected:
+                    client.publish("W/" + vrmID + "/settings/0/Settings/CGwacs/AcPowerSetPoint", '{"value":' + str(defaultGridSetpoint) + '}')
+                    logger.info("Current price is €" + '%.2f' % chargePriceNow + ". The average price today is €" + '%.2f' % averagePrice + ". This is higher than " + str(lowThreshold) + " *  daily average. This is not low enough to start charging or discharging. ")
+                    lastChargeCondition = 0
+            
+            client.loop_stop()
         else:
-        # If the ESS should not charge do this:              
-            if flagConntected:
-                client.publish("W/" + vrmID + "/settings/0/Settings/CGwacs/AcPowerSetPoint", '{"value":' + str(defaultGridSetpoint) + '}')
-                logger.info("Current price is €" + '%.2f' % chargePriceNow + ". The average price today is €" + '%.2f' % averagePrice + ". This is higher than " + str(lowChargeLimit) + " *  daily average. This is not low enough to start charging. ")
-                lastChargeCondition = 0
-        
-        client.loop_stop()
-    logger.info("Requirement has not changed. No MQTT message needed. ")     
+            logger.info("Charge requirement has not changed. No MQTT message needed. ")
+
+    if chargeMode == 2:
+        if dischargeConditionNow != lastDischargeCondition:
+            logger.info("Requirement has changed, sending MQTT message to change setpoint.")
+            # Control ESS over MQTT
+            client.connect(calculateBroker(vrmID), 8883, keepalive=60)
+            client.loop_start()
+            # Wait for connecting
+            while not flagConntected:
+                time.sleep(1)
+            if dischargeConditionNow:
+            # If the ESS should charge do this:        
+                if flagConntected:
+                    client.publish("W/" + vrmID + "/settings/0/Settings/CGwacs/AcPowerSetPoint", '{"value":' + str(-1*chargingGridSetpoint) + '}')
+                    logger.info("Current price is €" + '%.2f' % chargePriceNow + ". The average price today is €" + '%.2f' % averagePrice + ". This is higher than " + str(highThreshold) + " * daily average so the battery is now discjargomg.")
+                    lastChargeCondition = 1                
+            else:
+            # If the ESS should not charge do this:              
+                if flagConntected:
+                    client.publish("W/" + vrmID + "/settings/0/Settings/CGwacs/AcPowerSetPoint", '{"value":' + str(defaultGridSetpoint) + '}')
+                    logger.info("Current price is €" + '%.2f' % chargePriceNow + ". The average price today is €" + '%.2f' % averagePrice + ". This is higher than " + str(lowThreshold) + " *  daily average. This is not low enough to start charging or discharging. ")
+                    lastChargeCondition = 0
+            
+            client.loop_stop()
+        else:
+            logger.info("Discharge requirement has not changed. No MQTT message needed. ")           
 
 # Set up scheduler
 schedule.every().day.at("00:00:05").do(getPrices)
